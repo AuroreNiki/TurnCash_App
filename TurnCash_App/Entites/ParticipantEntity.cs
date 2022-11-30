acc74523619373ab8edb8bcfd88d358b23b34145
Nikiledji Aurore <auroranikiledji@gmail.com>
2022-11-30 12:52:51 +0100

    Creation de nouveau projet

---
+++ TurnCash_App/Entites/ParticipantEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnCash_App.Entites
+{
+    internal class ParticipantEntity
+    {
+        public int id_participant;
+        public string nom_participant;
+        public string email;
+        public string phone;
+
+        public ParticipantEntity(int id_participant, string nom_participant, string email, string phone)
+        {
+            this.id_participant = id_participant;
+            this.nom_participant = nom_participant;
+            this.email = email;
+            this.phone = phone;
+        }
+    }
+}
---
+++ TurnCash_App/Entites/PretEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnCash_App.Entites
+{
+    internal class PretEntity
+    {
+        public int Id_pret { get; set; }
+        public double montant;
+
+        public PretEntity(int id_pret, double montant)
+        {
+            Id_pret = id_pret;
+            this.montant = montant;
+        }
+    }
+}
